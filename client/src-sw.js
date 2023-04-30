01a4912fd81c0b29603b0a268fcbb0508e28cb49
User <michaelm810129@gmail.com>
2023-04-30 15:30:05 +0930

    Added SW/ added logic to install button

--- client/src-sw.js
+++ client/src-sw.js
@@ -7,6 +7,8 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+const offlineFallbackCacheName = 'offline-fallback-cache';
+
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -28,5 +30,39 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  // Finish this code. 
+  ({ request }) => request.mode === 'navigate',
+  async () => {
+    try {
+      const response = await fetch ('/');
+      return response;
+    } catch (error) {
+      const cachedResponse = await caches.match('/index.html');
+      if (cachedResponse) {
+        return cachedResponse;
+      } else {
+        const offlineFallbackResponse = await caches.match(
+          offlineFallbackCacheName
+        );
+        return offlineFallbackResponse;
+      }
+    }
+  }
 );
+
+// offlineFallback({
+//   cacheName: offlineFallbackCacheName,
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//   ],
+//   fallback: {
+//     url: '/index.html',
+//     cacheName: offlineFallbackCacheName,
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   },
+// });
